e2e08ae544d258cf84dff5e21c5f70b3eb8151e2
Eliseu Caputo <eliseujunior188@gmail.com>
2023-01-23 11:29:37 -0300

    update

--- processa_envio.php
+++ processa_envio.php
@@ -42,7 +42,7 @@
 	//print_r($mensagem);
 
 	if(!$mensagem->mensagemValida()) {
-		echo 'Mensagem é válida';
+		echo 'Mensagem não é válida';
 		die();
 	} 
 
@@ -51,32 +51,31 @@
 
 	try {
 	    //Server settings
-	    $mail->SMTPDebug = 2;                   //Enable verbose debug output
-	    $mail->isSMTP();                                            //Send using SMTP
-	    $mail->Host = 'smtp1.example.com;smatp2.example.com';                     //Set the SMTP server to send through
-	    $mail->SMTPAuth = true;                                   //Enable SMTP authentication
-	    $mail->Username = 'user@example.com';                     //SMTP username
-	    $mail->Password = 'secret';                               //SMTP password
-	    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+	    $mail->SMTPDebug = 2;                 				  //Enable verbose debug output
+	    $mail->isSMTP();                                      //Send using SMTP
+	    $mail->Host = 'smtp.gmail.com';                       //Set the SMTP server to send through
+	    $mail->SMTPAuth = true;                               //Enable SMTP authentication
+	    $mail->Username = 'eliseucaputo188@gmail.com';        //SMTP username
+	    $mail->Password = 'uecsaopaulo10';                    //SMTP password
+	    $mail->SMTPSecure = 'tls';            				  //Enable implicit TLS encryption
 	    $mail->Port = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
 	    //Recipients
-	    $mail->setFrom('from@example.com', 'Mailer');
-	    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-	    $mail->addAddress('ellen@example.com');               //Name is optional
-	    $mail->addReplyTo('info@example.com', 'Information');
-	    $mail->addCC('cc@example.com');
-	    $mail->addBCC('bcc@example.com');
+	    $mail->setFrom('eliseucaputo188@gmail.com', 'Web completo remetente');
+	    $mail->addAddress('eliseucaputo188@gmail.com', 'Web completo destinatário');     //Add a recipient
+	    //$mail->addReplyTo('info@example.com', 'Information');
+	    //$mail->addCC('cc@example.com');
+	    //$mail->addBCC('bcc@example.com');
 
 	    //Attachments
-	    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-	    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+	    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+	    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
 	    //Content
 	    $mail->isHTML(true);                                  //Set email format to HTML
-	    $mail->Subject = 'Here is the subject';
-	    $mail->Body = 'This is the HTML message body <b>in bold!</b>';
-	    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+	    $mail->Subject = 'Oi. Eu sou o assunto';
+	    $mail->Body = 'Oi. Eu sou o conteúdo do <strong>e-mail</strong>';
+	    $mail->AltBody = 'Oi. Eu sou o conteúdo do e-mail';
 
 	    $mail->send();
     	echo 'Message has been sent';
